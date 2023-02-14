2f35a1cb8aa3d4d7cdfd056fdad3da8bc9949d6a
Azigy <azigy@hotmail.com>
2023-02-14 18:29:15 -0500

    2-14-2023

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,4 +4,4 @@ from .models import Project # this statement import the models into our view are
 
 def home(request):
     projects = Project.objects.all()   # this grabs all the objects from our the DB into the home page
-    return  render(request,  'portfolio/home.html',  {'projects': projects}) # this is the variable from above
+    return  render(request,  'portfolio/home.html',  {'projects':projects}) # this is the variable from above
